1e5ec0dc8dd209faac5ae769b47a51048e5db5cf
Henrynunez112 <henrynunez@pursuit.org>
2020-09-23 12:06:57 -0400

    working on the about me page

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -1,51 +1,52 @@
 import React from "react";
 import Typical from "react-typical";
+import Henry2 from "../images/Henry2.JPG";
 import "./AboutMe.css";
 
 const AboutMe = () => {
   return (
     <div className="container aboutMeContainer">
       <div className="row aboutMeRow">
-        <div className="aboutMeLeftCol col-7">
-          <div className="jumbotron aboutMeJumbo">
-            <h3 className="display-5">Hey, my name is Henry Nuñez</h3>
-            <p className="lead">
-              I'm a <Typical
-                loop={Infinity}
-                wrapper="p"
-                steps={[
-                  "soccer player",
-                  500,
-                  "volleyball player",
-                  500,
-                  "art aficionado",
-                  500,
-                  "Hodureño",
-                  500,
-                  "Latinx",
-                  500,
-                  "Black",
-                  500,
-                  "Garifuna",
-                  500,
-                  "a son of two amazing individuals",
-                  500,
-                  "1 of 7 kids of those amazing individuals",
-                  500,
-                  "authentic",
-                  500,
-                  "team player",
-                  500,
-                  "simply me",
-                  500,
-                ]}
-              />
-            </p>
+        <div className="col-7 aboutMeJumbo">
+          <h1 className="display-4">Hey, I'm Henry Nuñez</h1>
+          <p className="h1 iAm">
+            I'm also a{" "}
+            <Typical
+              loop={Infinity}
+              wrapper="b"
+              steps={[
+                "Soccer Player",
+                500,
+                "Volleyball Player",
+                500,
+                "Art Aficionado",
+                500,
+                "Hondureño",
+                500,
+                "Runner",
+                500,
+                "Afro-Latinx",
+                500,
+                "Black Man",
+                500,
+                "Garifuna",
+                500,
+                "Authentic",
+                500,
+                "Team Player",
+                500,
+                "Creator",
+                500,
+              ]}
+            />
+          </p>
+          <div className='row resumeBtn'>
+            Hola
           </div>
         </div>
-        <div className="aboutMeRightCol col-5">
-          <div class="jumbotron meJumbo"></div>
-        </div>
+          <div class="col-5 meJumbo">
+          <img src={Henry2} alt={"henry is smilling"} class="img-thumbnail imgOfHenry"/>
+          </div>
       </div>
     </div>
   );

fa459ce37a8b3b0507d1e5e4ad63288ed5d6cf6e
Henrynunez112 <henrynunez@pursuit.org>
2020-09-26 18:04:42 -0400

    creating project css

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,10 +1,39 @@
 import React from "react";
+import Hwitter from "../images/Hwitter.png";
+import "./Projects.css";
 
-const Projects = () =>{
-    return (
-        <div>
-            Projects
+const Projects = () => {
+  return (
+    <div className="container projectsContainer">
+      <div className="row justify-content-center projectsHeader">
+        <div className="col-10 headerProjects">
+          <h1 class="display-4">
+            <span id="initials">P</span>rojects
+          </h1>
         </div>
-    )
-}
-export default Projects
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-10 projectJumbo">
+          <img src={Hwitter} className="img-fluid" id="hwitterImg" />
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-10 projectDescription">
+          <div className="row justify-content-start descriptionText">
+            <blockquote className="blockquote text-right">
+                
+              <p className="mb-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                posuere erat a ante.
+              </p>
+              <footer className="blockquote-footer">
+            
+              </footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Projects;
